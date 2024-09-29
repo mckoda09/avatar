419ddb0c1644f8665630df09c81b7fd174a6cc82
Aleksii Nikitin <mckoda09@gmail.com>
2024-09-29 22:04:15 +0500

    added footer

--- routes/index.tsx
+++ routes/index.tsx
@@ -8,6 +8,16 @@ export default function Home() {
       </div>
 
       <Avatar />
+
+      <div class="p-5 text-sm text-neutral-500 text-center">
+        <div>
+          Made with 💜 by{" "}
+          <a href="https://mckoda09.ru" class="underline">mckoda09</a>
+        </div>
+        <div>
+          <a href="https://github.com/mckoda09/avatar" class="underline">Source Code</a>
+        </div>
+      </div>
     </main>
   );
 }
